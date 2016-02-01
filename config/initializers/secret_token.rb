efcd2e45c851d420a86ef40d21d9ab406fdf199a
Jingchao Lee <satzcoal@hotmail.com>
2016-02-01 15:54:01 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Satzcoal::Application.config.secret_key_base = 'afdc5a45f45d4621c3c8b84e91aed8e663bcaf7d6f0b0ca3f6456e7003715a2a0c8a25185979608f5743b7c472040670f76a7c9e1a8961d983b801ffcd638572'
